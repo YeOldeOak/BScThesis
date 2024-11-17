a5ed89f016c6aaba5222680668f8e6fe25fbbc39
Arjen P. de Vries <arjen@acm.org>
2024-11-17 23:35:50 +0100

    Version with union and hand-coded IDs - takes ~6s wallclock time Version with join with query terms - takes ~13s wallclock time Both tested on home machine (~7 year old desktop) and Nov 5 2024 crawl data

--- bm25-late-groupby.sql
+++ bm25-late-groupby.sql
@@ -4,27 +4,27 @@
 --
 WITH queryterms AS (
   SELECT termid, df FROM dict WHERE 
-    (term = 'radboud' AND crange = cr('radboud'))
+    (term = 'radboud' AND crange = ows.cr('radboud'))
   UNION
   SELECT termid, df FROM dict WHERE 
-    (term = 'university' AND crange = cr('university'))
+    (term = 'university' AND crange = ows.cr('university'))
 ),
 termscores AS (
-  SELECT 
-    p.docid,
-    LOG((SELECT num_docs FROM stats) / (1 + q.df)) * 
-        (p.tf * (1.5 + 1)) / (p.tf + 1.5 * (1 - 0.75 + 0.75 * (d.len / (SELECT avgdl FROM stats)))
-    ) AS bm25termscore
-  FROM postings p, queryterms q, docs d
-  WHERE (p.termid = 11313573 AND p.thash = 29 AND p.termid = q.termid AND p.docid = d.docid)
-UNION
-  SELECT 
-    p.docid,
-    LOG((SELECT num_docs FROM stats) / (1 + q.df)) * 
-	(p.tf * (1.5 + 1)) / (p.tf + 1.5 * (1 - 0.75 + 0.75 * (d.len / (SELECT avgdl FROM stats)))
-    ) AS bm25termscore
-  FROM postings p, queryterms q, docs d
-  WHERE (p.termid =  9430643 AND p.thash = 25 AND p.termid = q.termid AND p.docid = d.docid)
+    SELECT 
+      p.docid,
+      LOG((SELECT num_docs FROM stats) / (1 + q.df)) * 
+          (p.tf * (1.5 + 1)) / (p.tf + 1.5 * (1 - 0.75 + 0.75 * (d.len / (SELECT avgdl FROM stats)))
+      ) AS bm25termscore
+    FROM postings p, queryterms q, docs d
+    WHERE (p.termid = 6609552) AND (p.thash = ows.p(6609552)) AND p.termid = q.termid AND (p.docid = d.docid)
+  UNION
+    SELECT 
+      p.docid,
+      LOG((SELECT num_docs FROM stats) / (1 + q.df)) * 
+  	(p.tf * (1.5 + 1)) / (p.tf + 1.5 * (1 - 0.75 + 0.75 * (d.len / (SELECT avgdl FROM stats)))
+      ) AS bm25termscore
+    FROM postings p, queryterms q, docs d
+    WHERE (p.termid = 7928317 AND p.thash = ows.p(7928317) AND p.termid = q.termid AND p.docid = d.docid)
 ),
 docscores AS (
   SELECT docid, sum(bm25termscore) AS bm25score
@@ -34,7 +34,8 @@ docscores AS (
 SELECT ds.docid, ds.bm25score, di.name
 FROM docscores ds
 JOIN docs di ON ds.docid = di.docid
-ORDER BY ds.bm25score DESC;
+ORDER BY ds.bm25score DESC
+LIMIT 10;
 
 --  JOIN queryterms q ON p.termid = q.termid AND p.thash = q.termid % 100 
 
