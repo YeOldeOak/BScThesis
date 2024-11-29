c40eed021b166be145c44b3956f10db8402ea8f6
Arjen P. de Vries <arjen@acm.org>
2024-11-29 17:44:08 +0100

    Minor changes for newer DuckDB (stricter on schema)

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- Using extra constraints on first character
 SELECT termid, df FROM dict WHERE 
-   (term = 'radboud') AND (crange=cr('radboud')) 
-OR (term = 'university') AND (crange=cr('university'));
+   (term = 'radboud') AND (crange=ows.cr('radboud')) 
+OR (term = 'university') AND (crange=ows.cr('university'));
 
 -- UNION variant
 SELECT termid, df FROM dict WHERE (term = 'radboud')
@@ -10,18 +10,18 @@ SELECT termid, df FROM dict WHERE (term = 'university');
 
 -- UNION variant with extra constraint
 SELECT termid, df FROM dict WHERE 
-  (term = 'radboud' AND crange = cr('radboud'))
+  (term = 'radboud' AND crange = ows.cr('radboud'))
 UNION
 SELECT termid, df FROM dict WHERE 
-  (term = 'university' AND crange = cr('university'));
+  (term = 'university' AND crange = ows.cr('university'));
 
 -- BM25 ranking
 WITH queryterms AS (
   SELECT termid, df FROM dict WHERE 
-    (term = 'radboud' AND crange = cr('radboud'))
+    (term = 'radboud' AND crange = ows.cr('radboud'))
   UNION
   SELECT termid, df FROM dict WHERE 
-    (term = 'university' AND crange = cr('university'))
+    (term = 'university' AND crange = ows.cr('university'))
 ),
 docscores AS (
   SELECT 
@@ -30,7 +30,7 @@ docscores AS (
 	LOG((SELECT num_docs FROM stats) / (1 + q.df)) * (p.tf * (1.5 + 1)) /
 	  (p.tf + 1.5 * (1 - 0.75 + 0.75 * (d.len / (SELECT avgdl FROM stats))))) AS bm25score
   FROM postings p, queryterms q, docs d
-  WHERE (p.termid = 11313573 AND p.thash = 29 AND p.termid = q.termid AND p.docid = d.docid)
+  WHERE (p.termid = 6609552) AND (p.thash = ows.p(6609552)) AND p.termid = q.termid AND (p.docid = d.docid)
   GROUP BY p.docid
 UNION
   SELECT 
@@ -39,13 +39,14 @@ UNION
 	LOG((SELECT num_docs FROM stats) / (1 + q.df)) * (p.tf * (1.5 + 1)) /
 	  (p.tf + 1.5 * (1 - 0.75 + 0.75 * (d.len / (SELECT avgdl FROM stats))))) AS bm25score
   FROM postings p, queryterms q, docs d
-  WHERE (p.termid =  9430643 AND p.thash = 25 AND p.termid = q.termid AND p.docid = d.docid)
+  WHERE (p.termid = 7928317 AND p.thash = ows.p(7928317) AND p.termid = q.termid AND p.docid = d.docid)
   GROUP BY p.docid
 )
 SELECT ds.docid, ds.bm25score, di.name
 FROM docscores ds
 JOIN docs di ON ds.docid = di.docid
-ORDER BY ds.bm25score DESC;
+ORDER BY ds.bm25score DESC
+LIMIT 20;
 
 --  JOIN queryterms q ON p.termid = q.termid AND p.thash = q.termid % 100 
 
