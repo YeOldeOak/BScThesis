3761c9f937608423559cd2160488a0fe8cd78cfa
Arjen P. de Vries <arjen@acm.org>
2024-12-01 15:57:14 +0100

    Fixes propagated to this query

--- bm25-todd.sql
+++ bm25-todd.sql
@@ -1,13 +1,16 @@
 -- Todd's BM25 ranking
 --
--- Did not exploit partitioning on thash
+-- Without using the term hash
 --
+
+USE rciff2;
+
 WITH queryterms AS (
   SELECT termid, df FROM dict WHERE 
-    (term = 'radboud' AND crange = cr('radboud'))
+    (term = 'radboud' AND crange = ows.cr('radboud'))
   UNION
   SELECT termid, df FROM dict WHERE 
-    (term = 'university' AND crange = cr('university'))
+    (term = 'university' AND crange = ows.cr('university'))
 ),
 docscores AS (
   SELECT 
@@ -15,24 +18,16 @@ docscores AS (
     SUM(
 	LOG((SELECT num_docs FROM stats) / (1 + q.df)) * (p.tf * (1.5 + 1)) /
 	  (p.tf + 1.5 * (1 - 0.75 + 0.75 * (d.len / (SELECT avgdl FROM stats))))) AS bm25score
-  FROM postings p, queryterms q, docs d
-  WHERE (p.termid = 11313573 AND p.termid = q.termid AND p.docid = d.docid)
-  GROUP BY p.docid
-UNION
-  SELECT 
-    p.docid,
-    SUM(
-	LOG((SELECT num_docs FROM stats) / (1 + q.df)) * (p.tf * (1.5 + 1)) /
-	  (p.tf + 1.5 * (1 - 0.75 + 0.75 * (d.len / (SELECT avgdl FROM stats))))) AS bm25score
-  FROM postings p, queryterms q, docs d
-  WHERE (p.termid =  9430643 AND p.termid = q.termid AND p.docid = d.docid)
+  FROM postings p
+  JOIN queryterms q ON p.termid = q.termid 
+  JOIN docs d ON p.docid = d.docid
   GROUP BY p.docid
 )
 SELECT ds.docid, ds.bm25score, di.name
 FROM docscores ds
 JOIN docs di ON ds.docid = di.docid
-ORDER BY ds.bm25score DESC;
+ORDER BY ds.bm25score DESC
+LIMIT 20;
 
---  JOIN queryterms q ON p.termid = q.termid AND p.thash = q.termid % 100 
 
 
