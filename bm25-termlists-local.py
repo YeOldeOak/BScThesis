cf2679ce576f4fe62f1c2d2d285963b21d47fb91
Todd Bellavia <toddibellavia@Yahoo.com>
2024-12-20 12:09:15 +0100

    latest termlists versions
    minor fixes, cleaned up comments

--- bm25-termlists-local.py
+++ bm25-termlists-local.py
@@ -5,22 +5,25 @@ import time
 
 
 
-con = duckdb.connect('ciff.db')
+# Load/create necessary views from local duckdb database file
+con = duckdb.connect('index.db')
 con.execute("CREATE OR REPLACE MACRO cr(term) AS ascii(term[1]) // 10;")
+con.execute("CREATE OR REPLACE VIEW postings AS SELECT * FROM index.owsdd.postings;")
+con.execute("CREATE OR REPLACE VIEW dict AS SELECT * FROM index.owsdd.dict;")
+con.execute("CREATE OR REPLACE VIEW docs AS SELECT * FROM index.owsdd.docs;")
+con.execute("CREATE OR REPLACE VIEW stats AS SELECT * FROM index.ows.stats;")
+
+# Random states for 10 queries was 37, for 100 queries it was 23, for 500 queries it was 66, for 50 it was 17
+# Load random sample of ORCAS queries to a parsable format
+queryamount = 500
+data = pd.read_csv(f'sample_queries{queryamount}.csv', header=None)
+queries = data.iloc[1:, 0].apply(lambda query: [term.replace("'", "''") for term in query.split()]).tolist()
 
 
-# Sample set of queries, each as a list of terms
-queries = [
-    ['radboud', 'university'],
-    ['research', 'data', 'science'],
-    ['open', 'access'],
-    ['artificial', 'intelligence', 'ethics'],
-    ['machine', 'learning', 'neural', 'network']]
 
+# Main function for building CTEs and running BM25 queries
 def run_queries(queries):
     for query in queries:
-         # Measure total time
-        start_time = time.time()
 
         # Build the queryterms CTE
         queryterms_cte = f"""
@@ -31,11 +34,19 @@ def run_queries(queries):
         )
         """
 
-        # Extract termids into a Python list
+        # Extract termids of query terms in current query into a Python list
         termids_result = con.execute(f"""
         SELECT termid FROM dict
         WHERE term IN ({", ".join(f"'{term}'" for term in query)});
         """).fetchall()
+
+        # Query terms without an id will return nothing in the lines above
+        # In the case an entire query returns nothing (eg. single term that doesn't have id), that is dealt with here
+        if not termids_result:
+            print(f"No terms found in dictionary for query: {query}")
+            continue
+
+        # Isolate the termids from the result
         termids = [row[0] for row in termids_result]
 
         # Construct the termscores CTE
@@ -48,9 +59,9 @@ def run_queries(queries):
                     ) AS bm25termscore
                 FROM postings p, queryterms q, docs d
                 WHERE (p.termid = {termid}
-                    AND p.thash = MOD(hash('{query[termids.index(termid)]}'), 40) 
+                    AND p.thash = MOD(hash({termid}), 40) 
                     AND p.termid = q.termid 
-                    AND p.docid = d.docid)"""    
+                    AND p.docid = d.docid)"""
             for termid in termids
         ])
 
@@ -71,19 +82,20 @@ def run_queries(queries):
         ORDER BY ds.bm25score DESC;
         """
 
-        
-
-        result = con.execute(f"EXPLAIN ANALYZE {scoreBM25}").fetchall()[0][1]
-
-         # Calculate the total execution time (this will include termid lookup and BM25 query)
-        end_time = time.time()
-        print(f"Total time: {end_time - start_time:.4f} seconds")
+        # This line is to be modified if actual values are to be returned. For timing purposes
+        # Explain analyze cannot be used, as it doesn't take into account, at the minimum, lookups for termids
+        result = con.execute(scoreBM25).fetchall()
 
 
-      
+
+# Measure total time
+start_time = time.time()     
 
 # Run all queries
 run_queries(queries)
+
+end_time = time.time()
+print(f"Total time: {end_time - start_time:.4f} seconds")
 
 # Close connection after use
 con.close()
--- bm25-termlists.py
+++ bm25-termlists.py
@@ -1,6 +1,9 @@
 import duckdb
 import time
-import re
+import pandas as pd
+
+
+
 
 # Connect to the DuckDB instance
 con = duckdb.connect()
@@ -10,27 +13,24 @@ with open('s3-init-priv.sql', 'r') as s3_conn:
     s3_privs = s3_conn.read()
     con.execute(s3_privs)
 
-# Create necessary views from the parquet files
+# Load/create necessary views from remote parquet files
 con.execute("CREATE OR REPLACE MACRO cr(term) AS ascii(term[1]) // 10;")
-con.execute("CREATE VIEW postings AS SELECT * FROM read_parquet('s3://todd/postings40/*/*');")
+con.execute("CREATE VIEW postings AS SELECT * FROM read_parquet('s3://todd/postings/*/*');")
 con.execute("CREATE VIEW dict AS SELECT * FROM read_parquet('s3://todd/pdict.parquet');")
 con.execute("CREATE VIEW docs AS SELECT * FROM read_parquet('s3://todd/documentData.parquet');")
 con.execute("CREATE VIEW stats AS SELECT * FROM read_parquet('s3://todd/docStats.parquet');")
 print("loaded")
 
+# Load random sample of ORCAS queries to a parsable format
+# random states for 10 queries was 37, for 100 queries it was 23, for 500 queries it was 66, for 50 it was 17
+queryamount = 100
+data = pd.read_csv(f'sample_queries{queryamount}.csv', header=None)
+queries = data.iloc[1:, 0].apply(lambda query: [term.replace("'", "''") for term in query.split()]).tolist()
 
-# Sample set of queries, each as a list of terms
-queries = [
-    ['radboud', 'university'],
-    ['research', 'data', 'science'],
-    ['open', 'access'],
-    ['artificial', 'intelligence', 'ethics'],
-    ['machine', 'learning', 'neural', 'network']]
 
+# Main function for building CTEs and running BM25 queries
 def run_queries(queries):
     for query in queries:
-         # Measure total time
-        start_time = time.time()
 
         # Build the queryterms CTE
         queryterms_cte = f"""
@@ -41,11 +41,19 @@ def run_queries(queries):
         )
         """
 
-        # Extract termids into a Python list
+        # Extract termids of query terms in current query into a Python list
         termids_result = con.execute(f"""
         SELECT termid FROM dict
         WHERE term IN ({", ".join(f"'{term}'" for term in query)});
         """).fetchall()
+
+        # Query terms without an id will return nothing in the lines above
+        # In the case an entire query returns nothing (eg. single term that doesn't have id), that is dealt with here
+        if not termids_result:
+            print(f"No terms found in dictionary for query: {query}")
+            continue
+
+        # Isolate the termids from the result
         termids = [row[0] for row in termids_result]
 
         # Construct the termscores CTE
@@ -58,7 +66,7 @@ def run_queries(queries):
                     ) AS bm25termscore
                 FROM postings p, queryterms q, docs d
                 WHERE (p.termid = {termid}
-                    AND p.thash = MOD(hash('{query[termids.index(termid)]}'), 40) 
+                    AND p.thash = MOD(hash({termid}), 40) 
                     AND p.termid = q.termid 
                     AND p.docid = d.docid)"""    
             for termid in termids
@@ -79,21 +87,24 @@ def run_queries(queries):
         FROM docscores ds
         JOIN docs di ON ds.docid = di.docid
         ORDER BY ds.bm25score DESC;
-        """
+        """    
 
-        
-
+        # print(scoreBM25)
+        # This line is to be modified if actual values are to be returned. For timing purposes
+        # Explain analyze cannot be used, as it doesn't take into account, at the minimum, lookups for termids
         result = con.execute(f"EXPLAIN ANALYZE {scoreBM25}").fetchall()[0][1]
+        formatted_result = result.replace("\\n", "\n")
+        print(formatted_result)
 
-         # Calculate the total execution time (this will include termid lookup and BM25 query)
-        end_time = time.time()
-        print(f"Total time: {end_time - start_time:.4f} seconds")
-
-
-      
+# Measure total time
+start_time = time.time()      
 
 # Run all queries
 run_queries(queries)
+
+# Formatted for minutes only
+end_time = time.time()
+print(f"Total time: {(end_time - start_time)/60:.4f} minutes")
 
 # Close connection after use
 con.close()
